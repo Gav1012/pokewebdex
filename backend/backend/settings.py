5bf3c584edaaeea203e380f47713bc21630f8459
Gav1012 <gavin.poley@gmail.com>
2024-07-19 20:23:08 -0700

    more changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +30,9 @@ SECRET_KEY = 'django-insecure-henk7#b3(!ifm0bw!etkmu1_wv*y^4!92)vju)6*=k%9%ti7ja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('PUBLIC_DNS'),
+]
 
 
 # Application definition
