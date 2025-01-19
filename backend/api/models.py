059e4d1c98de194c48f8c3a98c958446f8143840
Gav1012 <gavin.poley@gmail.com>
2025-01-18 23:51:24 -0800

    updated model for types and image, updated CSS for default colors

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(null=True, blank=True, max_length=50)
     pokedex_number = models.IntegerField(unique=True, null=True, blank=True)
+    type = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
 
     def __str__(self):
         return self.name
