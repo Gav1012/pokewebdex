8c117e93de2bd3ee6b4b8d75f2a74fd0da4e17a3
Gav1012 <gavin.poley@gmail.com>
2025-01-24 23:54:16 -0800

    updated css and dex file to change background depending on type

--- frontend/src/components/Dex/Dex.js
+++ frontend/src/components/Dex/Dex.js
@@ -12,7 +12,7 @@ export default function Dex({ pokemon, setPokemon }) {
         <div className="Dex">
             <div className="pokemon-list">
                 {pokemon.map((pokemon) => (
-                    <div className="single-pokemon">
+                    <div className={`single-pokemon type-${pokemon.type[0]}`}>
                         <h4 key={pokemon.pokedex_number} />
                         <p>{'#' + pokemon.pokedex_number + ' ' + pokemon.name}</p>
                         {' '} Dex Entry: {' '}
